4edc971c5a9a22170f334d0b1bb39df6af68e4d9
razorSharp <kirananto@gmail.com>
2017-01-03 23:03:08 +0530

    CODE OPTIMIZATION PART 2 - 3rd January

--- app/src/main/java/com/razor/lofo/PostsFragment.java
+++ app/src/main/java/com/razor/lofo/PostsFragment.java
@@ -27,7 +27,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.ads.AdRequest;
